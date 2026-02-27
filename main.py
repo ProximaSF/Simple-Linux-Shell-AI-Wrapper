1145f3c72640c69a5e18515043521163b813e25b
ProximaSF <proximafactor@gmail.com>
2026-02-27 03:13:43 -0500

    minor change

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import time
-import boto3
+import inspect
 from dotenv import load_dotenv
+import boto3
 from botocore.exceptions import ClientError
 
 import sys
@@ -35,7 +36,7 @@ class AI_Assistant:
     def ai_content(self):
         is_model = [model for model in MODELS_IDS if self.model_id.lower() in model.lower()]
         if is_model:
-            if self.model_id == DEFAULT_MODEL_ID:
+            if is_model[0] == DEFAULT_MODEL_ID:
                 result = deepseek3_2((LOG_FILE), self.prompt, self.model_id, self.token_limit, self.client)
                 return result
             elif "claude" in is_model[0]:
@@ -67,13 +68,13 @@ def parse_args(arglist):
 if __name__ == "__main__":
     args = parse_args(sys.argv[1:])
     if args.verbose:
-        msg = f"""
+        msg = inspect.cleandoc(f"""
             Verbose enabled (python):
             ------------------------------------------
             Using Model: {args.model}
             Prompt: {args.ai_prompt}
             Token Limit: {args.token}
-            """
+            """)
         print(msg)
 
     main(args.ai_prompt, args.token, args.model)
